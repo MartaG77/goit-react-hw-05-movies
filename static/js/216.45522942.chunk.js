2e2b68cb22a390c6c98ddf2bbcea825dffc6c655
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2024-02-25 16:00:07 +0000

    Deploying to gh-pages from @ MartaG77/goit-react-hw-05-movies@a456e095c3d77efa49362a1f8453d89f75b80f33 🚀

---
+++ static/js/216.45522942.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[216],{216:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("div",{children:"Sorry, page not found."})}}}]);
+//# sourceMappingURL=216.45522942.chunk.js.map
